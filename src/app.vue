742750350aa0c8dfef68b5d0910c0da11e359442
PabloYza <P.deyzaguirre13@gmail.com>
2019-09-17 10:06:27 +0200

    AppHeader component + boiler plate

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AppHeader />
+  </div>
+</template>
+
+
+<script>
+import AppHeader from './components/AppHeader';
+
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+    }
+}
+</script>
