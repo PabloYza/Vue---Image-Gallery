f6583322ab8c088583bd7c8bf9973fa52a14359e
PabloYza <P.deyzaguirre13@gmail.com>
2019-09-18 09:55:22 +0200

    VueRouter initial setUp

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './app';
 import store from './store';
+import AuthHandler from './components/AuthHandler';
 
-new Vue ({
+// ROUTES
+Vue.use(VueRouter); // initial "handshake" between Vue and VueRouter
+const router = new VueRouter ({
+  routes: [
+    { path: '/oauth2/callback', component: AuthHandler}, // Path we want this RULE to define
+  ]
+});
+
+
+// Vue Instance
+new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app');
